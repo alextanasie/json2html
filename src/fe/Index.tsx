f5b0b2b5362fef6a7de6c1c8a849882647d34281
Alex Tanasie <18068016+alextanasie@users.noreply.github.com>
2024-05-10 16:29:55 +0300

    feat: initial commit with working app

---
+++ src/fe/Index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { createHTML } from "../helpers/html.helpers";
+import { generateFontFamilyFetchLink } from "../helpers/getFontFamilies";
+
+// note: this is a minimal setup, clearly we should take html/head/body out into a parent component etc
+export function Index({ jsonDefinition }: { jsonDefinition: string }) {
+    const generatedJsx = createHTML(jsonDefinition);
+    const fontRepo = process.env.FONT_REPO;
+
+    // fontRepo can't be null, if we ensure on build time that env is set correctly
+    const fontsUrl = generateFontFamilyFetchLink(jsonDefinition, fontRepo!);
+
+    return (
+        <html>
+            <head>
+                {
+                    fontsUrl && <link rel="stylesheet" type="text/css" href={fontsUrl} />
+                }
+            </head>
+            <body style={resetStyles}>
+                <div className="banner-container">
+                    {generatedJsx}
+                </div>
+            </body>
+        </html>
+    )
+}
+
+// Reset styles
+const resetStyles = {
+    margin: 0,
+    padding: 0,
+    border: 0,
+    fontSize: '100%',
+    font: 'inherit',
+    verticalAlign: 'baseline',
+    display: 'block',
+    lineHeight: 1,
+};
---
+++ src/server.ts
@@ -0,0 +1,55 @@
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+
+import React, { createElement } from 'react';
+import { createHTML } from './helpers/html.helpers';
+import { renderToString } from 'react-dom/server';
+import { Index } from './fe/Index';
+
+// note: combining import and require is not recommended, but having some issue with the ES6 fetch module
+const fetch = require('node-fetch');
+
+
+import 'dotenv/config';
+
+const app = express();
+const port = 3000;
+
+app.get('/app', async (req, res) => {
+    if (!req.query.hash) {
+        return res.status(400).send('Bad Request: Missing q parameter');
+    }
+
+    // TODO sanitize and make sure JSON_REPO exists in the env before starting the app. Don't allow app start if env variable does not exist
+    const jsonUrl = process.env.JSON_REPO?.replace('{hash}', req.query.hash as string);
+
+    if (!jsonUrl) {
+        // TODO use a logging library
+        console.error('Cannot fetch json!', req);
+        return res.status(500).send('Internal Server Error');
+    }
+    const jsonDefinition = await fetch(jsonUrl);
+    const jsonDefinitionText = await jsonDefinition.text();
+
+    const indexComponent: React.ComponentType<{ jsonDefinition: string }> = Index;
+    const appString = renderToString(createElement(indexComponent, { jsonDefinition: jsonDefinitionText }));
+
+    return res.send(appString);
+});
+
+// app.get('/banner', async (req, res) => {
+//     fs.readFile(path.join(__dirname, 'example.json'), 'utf8', (err, data) => {
+//         if (err) {
+//             console.error(err);
+//             res.status(500).send('An error occurred while reading the file.');
+//         } else {
+//             const resultHtmlFilePath = createHTML(data);
+//             res.sendFile(resultHtmlFilePath);
+//         }
+//     });
+// });
+
+app.listen(port, () => {
+    console.log(`jsonToHtml is running at http://localhost:${port}`);
+});
